9931c4c60ee395b72064607c5acf118bdf5aa356
Zifei.David.Zhong <zhongz@email.sc.edu>
2026-01-02 11:37:52 -0500

    fibonacci.c: release memory

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int fibonacci(int n)
+{
+    if (n == 0) { return 0; }
+
+    int memory_size = (n+1) * 4;
+    int *arr = (int *) malloc(memory_size);
+
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for (int i = 2; i <= n; ++i)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    int s = arr[n];
+
+    // release memory    
+    free(arr);
+    arr = NULL;
+
+    return s;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -12,8 +36,25 @@ int main(int argc, char *argv[])
 
     printf("Your input is: %d\n", n);
 
-    printf("I will calculate the %d-th Fibonacci number for you!\n", n);
+    if (n < 0)
+    {
+        printf("Please input an integer >= 0.\n");
+        return 1;
+    }
 
+    int f = fibonacci(n);
+
+    if (n == 1)
+    {
+        /* TODO: What if n = 2 or 3 */
+
+        printf("The 1st Fibonacci number is: %d\n", f);
+    } 
+    else 
+    {
+        printf("The %d-th Fibonacci number is: %d\n", n, f);
+    }
+    
     return 0;
 }
 
