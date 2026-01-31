fe08295413417d014590e83cd29a06006e800842
Zifei.David.Zhong <zhongz@email.sc.edu>
2026-01-31 12:01:31 -0500

    insert an integer into a sorted array

--- insertion.c
+++ insertion.c
@@ -2,6 +2,34 @@
 #include<stdlib.h>
 #include<string.h>
 
+int array_insert(int *b, int len, int y)
+{
+    /* insert y into array b[] */
+
+    // 1) find the location (index j)
+    int j = 0;
+    for(int i=0; i<len; ++i)
+    {
+        if(b[i] > y)
+        {
+            j=i; break;   
+        }
+    }
+    printf("j=%d\n", j);
+
+    // 2) shiftb[j],b[j+1],..., to the right
+    for(int i=len-2; i>=j; --i)
+    {
+        b[i+1] = b[i];
+    }
+
+    // 3) b[i] = y;
+    b[j] = y;
+
+    return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
     /* arg 1: comma separated sorted numbers like 1,3,5,17,29,105
@@ -32,7 +60,7 @@ int main(int argc, char *argv[])
     int length = comma_count + 1;
     
     // allocate memory for the array of integers
-    int *a = (int *) malloc(length * 4);
+    int *a = (int *) malloc((length+1) * 4);
     
     // put the first number into a[0]
     a[0] = atoi(&argv[1][0]);
@@ -48,12 +76,22 @@ int main(int argc, char *argv[])
         }
     }
 
-    for(int i=0; i<length; ++i)
+    for(int i=0; i<length+1; ++i)
     {
-       printf("%d\n", a[i]);
+       printf("%d ", a[i]);
     }
+    printf("\n");
+
     
     /* Let's do our insetion here */
+    int x = atoi(argv[2]);
+    array_insert(a, length+1, x);
+    
+    for(int i=0; i<length+1; ++i)
+    {
+       printf("%d ", a[i]);
+    }
+    printf("\n");
 
     // Free the memory
     free(a); a = NULL;
